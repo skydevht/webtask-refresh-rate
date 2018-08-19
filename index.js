fe15bdb34950203effd6a1d539c1d232f6799e15
Hash Skyd <skydhash@gmail.com>
2018-08-19 00:32:03 -0400

    added runner and comments

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
+const http = require('http');
 const refreshRate = require('./refreshRate');
 
+// Mocking the context object and its child storage
 
-console.log(refreshRate);
+const Storage = function() {
+  const setData = (data, options, callback) => {
+    this.data = data;
+    callback(null);
+  }
+
+  const getData = (callback) => {
+    callback(null, this.data);
+  }
+
+  this.set = setData;
+  this.get = getData;
+}
+
+const context = {
+  storage: new Storage(),
+};
+
+// Now set a server real quick
+const port = 3000;
+const server = http.createServer((req, res) => refreshRate(context, req, res));
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+})
+
+
--- refreshRate.js
+++ refreshRate.js
@@ -1,17 +1,21 @@
 'use latest';
-const cheerio = require('cheerio');
+/*
+ * Only this file is necessary if you're going to use it as a webtask severless function
+ *
+ */
+const cheerio = require('cheerio'); // html parser
 const rp = require('request-promise').defaults({
   transform: function(body) {
-    return cheerio.load(body);
+    return cheerio.load(body); // here instead of every time in the function
   },
   headers: {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36' // For BUH
   },
-  jar: true
+  jar: true, // For unibank
 });
 
 // Haitian Gourdes is the reference and its value is set to 1
-
+// Just some constants to help identify them, client side (and lighter too)
 const ids = {
   haitianGourde: 1,
   usInternational: 2,
@@ -32,30 +36,44 @@ const ids = {
   canadianSogebankDollar: 11,
 };
 
+// We do nothing, really
 function handleScrapingError(err) {
   console.log('Error:', err);
 }
 
+// This function just send the error to the client
 function sendError(err, res) {
   res.writeHead(500, { 'Content-Type': 'application/json'});
   res.end(JSON.stringify(err));
 }
 
+// Tired of writing those too lines of code so we make it dry
 function sendData(data, res) {
   res.writeHead(200, { 'Content-Type': 'application/json'});
   res.end(JSON.stringify(data));
 }
 
+// The main function
+// @param ctx : An object provided by Webtask api
 function refreshRate(ctx, req, res) {
+  // In webtask the cron task call the endpoint using POST
+  // and anyone else use GET, so it's a quicky way to differentiate the two
   if (req.method === 'GET') {
+    // GET request, everyone, that's why the cache
     ctx.storage.get(function(error, data) {
       if (error) sendError(error, res);
       else sendData(data, res);
     })
   } else {
+    // POST request, either cron or manual
+    // We use promise a lot
     global.Promise.all([
-      rp('https://www.unibankhaiti.com/wp-content/themes/unibank/s2.php')
+      // So UNIBANK store a cookie with a session id (why?), if it does not find
+      // it goes to this link and store one, then redirect back to the main page
+      // So we go there first, store the cookie, then ask for the home page
+      rp('https://www.unibankhaiti.com/wp-content/themes/unibank/s2.php') // gimme a cookie, you
       .then(function() {
+        // now the real thing
         return rp('https://www.unibankhaiti.com/').then(function($) {
           const brhRate = Number($('#right > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > span').text().split(' ')[0]);
           const unibankBuyRate = Number($('#login > div > div > table > tbody > tr:nth-child(2) > td:nth-child(2) > strong').text().split(' ')[0])
@@ -158,6 +176,7 @@ function refreshRate(ctx, req, res) {
         ]);
       }, handleScrapingError)
     ]).then(function(r){
+      // so r is an array of the arrays returned by the functions above, so we flatten them and save them in the context
       const rates = r.reduce(function(acc, val) { return acc.concat(val); }, []);
       ctx.storage.set(rates, {force: 1}, function(err) {
         if (err) sendError(err, res);
